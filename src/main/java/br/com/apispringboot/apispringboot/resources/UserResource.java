96efe22ce79fd70ab0ba451e40dc1a27ce4c3444
Gustavo Franceschini de Araujo <gustavofranceschini22@gmail.com>
2023-08-01 14:57:40 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/apispringboot/apispringboot/resources/UserResource.java
+++ src/main/java/br/com/apispringboot/apispringboot/resources/UserResource.java
@@ -1,14 +1,17 @@
 package br.com.apispringboot.apispringboot.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.apispringboot.apispringboot.domain.User;
 import br.com.apispringboot.apispringboot.dto.UserDTO;
@@ -24,17 +27,32 @@ public class UserResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = userService.findAll();
-		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		List<UserDTO> listDto = list
+				.stream()
+				.map(x -> new UserDTO(x))
+				.collect(Collectors.toList());
+		
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		
 		User obj = userService.findById(id);
 		
-		
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/br/com/apispringboot/apispringboot/services/UserService.java
+++ src/main/java/br/com/apispringboot/apispringboot/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.apispringboot.apispringboot.domain.User;
+import br.com.apispringboot.apispringboot.dto.UserDTO;
 import br.com.apispringboot.apispringboot.repository.UserRepository;
 import br.com.apispringboot.apispringboot.services.exceptions.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
-;
