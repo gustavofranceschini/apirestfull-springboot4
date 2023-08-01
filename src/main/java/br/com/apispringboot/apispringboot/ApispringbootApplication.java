9026b00af9b6989f4dcfb75b81b573ec60f2ae5a
Gustavo Franceschini de Araujo <gustavofranceschini22@gmail.com>
2023-08-01 09:58:50 -0300

    Created Project

---
+++ src/main/java/br/com/apispringboot/apispringboot/ApispringbootApplication.java
@@ -0,0 +1,12 @@
+package br.com.apispringboot.apispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispringbootApplication.class, args);
+	}
+}
