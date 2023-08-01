935522f9e3b6c076f825c0bffb717d644c35a35e
Gustavo Franceschini de Araujo <gustavofranceschini22@gmail.com>
2023-08-01 17:56:05 -0300

    Consulta simples com @Query

--- src/main/java/br/com/apispringboot/apispringboot/repository/PostRepository.java
+++ src/main/java/br/com/apispringboot/apispringboot/repository/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.apispringboot.apispringboot.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.apispringboot.apispringboot.domain.Post;
@@ -10,6 +11,11 @@ import br.com.apispringboot.apispringboot.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 
 }
